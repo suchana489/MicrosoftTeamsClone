894d7ba7342a276ab0ff9f38e112bfa9e30b4de3
suchana489 <85669123+suchana489@users.noreply.github.com>
2021-07-09 23:27:09 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -8,7 +8,7 @@ myVideo.muted = true;
 
 var peer = new Peer(undefined, {
   path: "/peerjs",
-  host: "localhost",
+  host: "'peerjs-server.herokuapp.com'",
   port: "process.env.PORT",
 });
 
@@ -223,7 +223,7 @@ const shareScreen = async () => {
   
   const myPeer = new Peer(undefined, {
     path: '/peerjs',
-    host: 'localhost',
+    host: 'peerjs-server.herokuapp.com',
     port: 'process.env.PORT'
   })
 const myVideo2 = document.createElement('video')
