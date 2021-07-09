d9845e00f7a8c1b34efb3f0b2f6fd267639704e8
suchana489 <85669123+suchana489@users.noreply.github.com>
2021-07-09 19:19:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ const { v4: uuidV4 } = require('uuid')
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use('/peerjs', peerServer);
-app.ge', (req, res) => {
+app.get('/', (req, res) => {
   res.redirect(`/${uuidV4()}`)
 })
 
