9a4b45cccae049c96aaff433f5399953f71e4879
suchana489 <85669123+suchana489@users.noreply.github.com>
2021-07-11 10:34:24 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const socket = io("/");
+/*const socket = io("/");
 const chatInputBox = document.getElementById("chat_message");
 const all_messages = document.getElementById("all_messages");
 const main__chat__window = document.getElementById("main__chat__window");
@@ -214,7 +214,7 @@ const setStopShareScreen = () => {
   document.getElementById("playPauseVideo").innerHTML = html;
 };
 */
-const shareScreen = async () => {
+/*const shareScreen = async () => {
 
 
 
@@ -293,3 +293,71 @@ function close_window() {
    
   
   }
+*/
+const socket = io('/');
+const peer = new Peer();
+let myVideoStream;
+let myId;
+var videoGrid = document.getElementById('videoDiv')
+var myvideo = document.createElement('video');
+myvideo.muted = true;
+const peerConnections = {}
+navigator.mediaDevices.getUserMedia({
+  video:true,
+  audio:true
+}).then((stream)=>{
+  myVideoStream = stream;
+  addVideo(myvideo , stream);
+  peer.on('call' , call=>{
+    call.answer(stream);
+      const vid = document.createElement('video');
+    call.on('stream' , userStream=>{
+      addVideo(vid , userStream);
+    })
+    call.on('error' , (err)=>{
+      alert(err)
+    })
+    call.on("close", () => {
+        console.log(vid);
+        vid.remove();
+    })
+    peerConnections[call.peer] = call;
+  })
+}).catch(err=>{
+    alert(err.message)
+})
+peer.on('open' , (id)=>{
+  myId = id;
+  socket.emit("newUser" , id , roomID);
+})
+peer.on('error' , (err)=>{
+  alert(err.type);
+});
+socket.on('userJoined' , id=>{
+  console.log("new user joined")
+  const call  = peer.call(id , myVideoStream);
+  const vid = document.createElement('video');
+  call.on('error' , (err)=>{
+    alert(err);
+  })
+  call.on('stream' , userStream=>{
+    addVideo(vid , userStream);
+  })
+  call.on('close' , ()=>{
+    vid.remove();
+    console.log("user disconect")
+  })
+  peerConnections[id] = call;
+})
+socket.on('userDisconnect' , id=>{
+  if(peerConnections[id]){
+    peerConnections[id].close();
+  }
+})
+function addVideo(video , stream){
+  video.srcObject = stream;
+  video.addEventListener('loadedmetadata', () => {
+    video.play()
+  })
+  videoGrid.append(video);
+}
